1d20ad4db05d36c685e4d41fcd2d6acb67c489b6
Alberto Perugini <albertoperugini.ap@gmail.com>
2022-05-25 13:51:47 +0200

    various fixes

--- code/server/unit_test/InternalOrderServices.test.js
+++ code/server/unit_test/InternalOrderServices.test.js
@@ -42,7 +42,7 @@ describe('InternalOrder services no err tests', () => {
 
     testCreateInternalOrder();
     getInternalOrders();
-    // getInternalOrdersByState();
+    getInternalOrdersByState();
 
 });
 
