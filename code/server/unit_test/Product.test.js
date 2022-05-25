4c691feacb8a7c84bacf46d3009a664b8bb17b2f
Alberto Perugini <albertoperugini.ap@gmail.com>
2022-05-25 16:28:59 +0200

    product tests

---
+++ code/server/unit_test/Product.test.js
@@ -0,0 +1,42 @@
+'use strict';
+const DB = require('../Modules/DB').DB;
+const db = new DB(':memory:');
+const Product = require('../Modules/Product');
+const pr_dao = new Product(db.db);
+
+const prod1 = {
+    "SKUId": 12,
+    "description": "a product",
+    "price": 10.99,
+    "qty": 3
+}
+
+describe('Product dao tests', () => {
+    beforeEach(async () => {
+        await db.dropTableProduct();
+        await db.createTableProduct();
+    });
+
+    testInsertAndGetProductRO();
+
+});
+
+function testInsertAndGetProductRO() {
+    test('test product', async () => {
+
+        await pr_dao.insertProductReturnOrder(prod1.SKUId, prod1.description, prod1.price, 1)
+
+        let res = await pr_dao.getProducts();
+        expect(res.length).toStrictEqual(1);
+        expect(res[0].SKUId).toStrictEqual(prod1.SKUId);
+        expect(res[0].description).toStrictEqual(prod1.description);
+        expect(res[0].price).toStrictEqual(prod1.price);
+
+        res = await pr_dao.deleteProductByReturnOrderId(1);
+
+        res = await pr_dao.getProducts();
+        expect(res.length).toStrictEqual(0);
+
+
+    });
+}
